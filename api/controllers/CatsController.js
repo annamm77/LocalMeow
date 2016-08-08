4c386c206859c772d4f4e8dd819e2fe1ec8a757e
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-08 16:54:03 -0700

    attempt to get controller to pass variables to view

--- api/controllers/CatsController.js
+++ api/controllers/CatsController.js
@@ -4,10 +4,30 @@
  * @description :: Server-side logic for managing cats
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+var request = require('request');
 
 module.exports = {
 	index: function (req, res) {
-    res.view();
+		console.log("hello from the index")
+
+		request('http://api.petfinder.com/pet.find?key=SECRETKEYHERE&animal=cat&location=98005', function (error, response, body) {
+			console.log("I'm in the request in the index!!!")
+			if (!error && response.statusCode == 200) {
+		    console.log(body)
+				return res.view('cats/index',{
+			    hottestOvens: ovens
+			  });
+		  }
+		)
+
 }
+
+	// index: function(req, res) {
+	// 		Applicant.findAll().done(function(err, applicants) {
+	// 				res.view({
+	// 						apps: applicants
+	// 				});
+	// 		});
+	// }
 
 };

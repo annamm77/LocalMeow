2ce239974d70961724368cf6e7fdc6cf35bd677b
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-08 22:07:12 -0700

    controller is fetching api data and displaying json result on page

--- api/controllers/CatsController.js
+++ api/controllers/CatsController.js
@@ -3,16 +3,13 @@ var request = require('request');
 module.exports = {
 
 	index: function (req, res) {
-		return res.view('index', {
-			test: "boop"
-		});
+
+		request('http://api.petfinder.com/pet.find?key=7ed5e3e2b82b565a0578223bb4e0ba92&animal=cat&location=98005&format=json', function (error, response, body) {
+		  if (!error && response.statusCode == 200) {
+				return res.view('index', {
+					test: body
+				});
+		  }
+		})
 	}
-
-
-	// request('http://api.petfinder.com/pet.find?key=KEYHERE&animal=cat&location=98005', function (error, response, body) {
-	//   if (!error && response.statusCode == 200) {
-	//     console.log(body)
-	//   }
-	// })
-
 };

ae1f6c53645fdb948badbb7c2ac81af820a8e26d
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-08 22:26:30 -0700

    controller can make api requests and display response in index

--- api/controllers/CatsController.js
+++ api/controllers/CatsController.js
@@ -1,10 +1,11 @@
 var request = require('request');
+var dotenv = require('dotenv');
+dotenv.load();
 
 module.exports = {
 
 	index: function (req, res) {
-
-		request('http://api.petfinder.com/pet.find?key=7ed5e3e2b82b565a0578223bb4e0ba92&animal=cat&location=98005&format=json', function (error, response, body) {
+		request('http://api.petfinder.com/pet.find?key=' + process.env.API_KEY + '&animal=cat&location=98005&format=json', function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
 				return res.view('index', {
 					test: body

8f07059593d26cdcac1c540ce600d71a63e24fde
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-09 13:29:27 -0700

    generated cat model

---
+++ api/models/Cat.js
@@ -0,0 +1,41 @@
+/**
+ * Cat.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    [id : { type: 'integer,' },
+
+    name : { type: 'string,' },
+
+    age : { type: 'string,' },
+
+    sex : { type: 'string,' },
+
+    breed : { type: 'string,' },
+
+    shelterid : { type: 'string,' },
+
+    description : { type: 'string,' },
+
+    image : { type: 'string,' },
+
+    address : { type: 'string,' },
+
+    city : { type: 'string,' },
+
+    state : { type: 'string,' },
+
+    zip : { type: 'string,' },
+
+    phone : { type: 'string,' },
+
+    email : { type: 'string]' }
+  }
+};
+

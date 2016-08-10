c7c937f2fe82c7db56d79eab9910b47dcf56df57
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-10 11:54:23 -0700

    trying again with request-promise. returns empty obj

--- api/controllers/CatsController.js
+++ api/controllers/CatsController.js
@@ -6,12 +6,16 @@ module.exports = {
 
 	index: function (req, res) {
 		var PetFinderWrapper = require('../../PetFinderWrapper.js');
-		console.log("I'm the PetFinderWrapper and I have this many keys: " + Object.keys(PetFinderWrapper).length)
-		console.log(PetFinderWrapper)
+		// console.log("I'm the PetFinderWrapper and I have this many keys: " + Object.keys(PetFinderWrapper).length)
+		// console.log("I'm this long: " + PetFinderWrapper.length)
+		// console.log("This is what I am: " + PetFinderWrapper)
+		// console.log("If I'm working, this'll be a cat object: " + PetFinderWrapper[0])
+		// console.log(PetFinderWrapper)
+
 		request('http://api.petfinder.com/pet.find?key=' + process.env.API_KEY + '&animal=cat&location=98005&format=json', function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
 				return res.view('index', {
-					searchresults: body
+					searchresults: PetFinderWrapper
 				});
 		  }
 		})

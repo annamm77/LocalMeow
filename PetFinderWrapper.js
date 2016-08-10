a56b8170d5ee82b256940a2ca2a2aac0f80823d9
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-10 15:40:58 -0700

    refactored request-promise and data transform logic into wrapper. controller returns object of all pets

--- PetFinderWrapper.js
+++ PetFinderWrapper.js
@@ -7,45 +7,45 @@ var options = {
   headers: {
         'User-Agent': 'Request-Promise'
     },
- json: true
+ json: true,
+ transform2xxOnly: false,
+ transform: function (response) {
+   var allpets = response.petfinder.pets.pet
+
+   var petsarray = []
+
+   for (var i = 0, len = allpets.length; i < len; i++) {
+     var obj = {}
+
+     obj["id"] = allpets[i].id.$t
+     obj["name"] = allpets[i].name.$t
+     obj["age"] = allpets[i].age.$t
+     obj["sex"] = allpets[i].sex.$t
+     obj["breed"] = allpets[i].breeds.breed.$t
+     obj["shelterid"] = allpets[i].shelterId.$t
+     obj["description"] = allpets[i].description.$t
+
+     if (Object.getOwnPropertyNames(allpets[i].media).length === 0) {
+       continue
+     } else {
+       obj["image"] = allpets[i].media.photos.photo[2].$t
+     }
+
+     obj["address"] = allpets[i].contact.address1.$t
+     obj["city"] = allpets[i].contact.city.$t
+     obj["state"] = allpets[i].contact.state.$t
+     obj["zip"] = allpets[i].contact.zip.$t
+     obj["phone"] = allpets[i].contact.phone.$t.trim()
+     obj["email"] = allpets[i].contact.email.$t
+
+     petsarray.push(obj)
+   }
+   return petsarray
+ }
 }
 
-rp(options)
-  .then(function (repos) {
-    var allpets = repos.petfinder.pets.pet
-
-    var petsarray = []
-
-    for (var i = 0, len = allpets.length; i < len; i++) {
-      var obj = {}
-
-      obj["id"] = allpets[i].id.$t
-      obj["name"] = allpets[i].name.$t
-      obj["age"] = allpets[i].age.$t
-      obj["sex"] = allpets[i].sex.$t
-      obj["breed"] = allpets[i].breeds.breed.$t
-      obj["shelterid"] = allpets[i].shelterId.$t
-      obj["description"] = allpets[i].description.$t
-
-      if (Object.getOwnPropertyNames(allpets[i].media).length === 0) {
-        continue
-      } else {
-        obj["image"] = allpets[i].media.photos.photo[2].$t
-      }
-
-      obj["address"] = allpets[i].contact.address1.$t
-      obj["city"] = allpets[i].contact.city.$t
-      obj["state"] = allpets[i].contact.state.$t
-      obj["zip"] = allpets[i].contact.zip.$t
-      obj["phone"] = allpets[i].contact.phone.$t.trim()
-      obj["email"] = allpets[i].contact.email.$t
-
-      petsarray.push(obj)
-    }
-    return petsarray
-  })
-  .catch(function (err) {
-    console.log("That did not work not even a BIT" + err);
-  })
-
-// module.exports = rp(options)
+module.exports = {
+  getallpets: function () {
+    return rp(options)
+  }
+}
--- api/controllers/CatsController.js
+++ api/controllers/CatsController.js
@@ -1,56 +1,15 @@
-var rp = require('request-promise')
-var dotenv = require('dotenv');
-dotenv.load();
+var petfinderwrapper = require('../../PetFinderWrapper')
 
 module.exports = {
 
 	index: function (req, res) {
-		var options = {
-		  uri: 'http://api.petfinder.com/pet.find?key=' + process.env.API_KEY + '&animal=cat&location=98005&format=json',
-		  headers: {
-		        'User-Agent': 'Request-Promise'
-		    },
-		 json: true
-		}
-
-		rp(options)
-		  .then(function (repos) {
-		    var allpets = repos.petfinder.pets.pet
-
-		    var petsarray = []
-
-		    for (var i = 0, len = allpets.length; i < len; i++) {
-		      var obj = {}
-
-		      obj["id"] = allpets[i].id.$t
-		      obj["name"] = allpets[i].name.$t
-		      obj["age"] = allpets[i].age.$t
-		      obj["sex"] = allpets[i].sex.$t
-		      obj["breed"] = allpets[i].breeds.breed.$t
-		      obj["shelterid"] = allpets[i].shelterId.$t
-		      obj["description"] = allpets[i].description.$t
-
-		      if (Object.getOwnPropertyNames(allpets[i].media).length === 0) {
-		        continue
-		      } else {
-		        obj["image"] = allpets[i].media.photos.photo[2].$t
-		      }
-
-		      obj["address"] = allpets[i].contact.address1.$t
-		      obj["city"] = allpets[i].contact.city.$t
-		      obj["state"] = allpets[i].contact.state.$t
-		      obj["zip"] = allpets[i].contact.zip.$t
-		      obj["phone"] = allpets[i].contact.phone.$t.trim()
-		      obj["email"] = allpets[i].contact.email.$t
-
-		      petsarray.push(obj)
-		    }
+		petfinderwrapper.getallpets().then(function(petsarray){
 				return res.view('index', {
 					searchresults: petsarray
 				});
 		  })
 		  .catch(function (err) {
-		    console.log("That did not work not even a BIT" + err);
+				return res.view('404')
 		  })
 	}
 };

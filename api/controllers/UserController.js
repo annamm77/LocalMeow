2c56f2122d9f7a72ebfdee57169ce7d4f867c30e
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-18 16:05:35 -0700

    user gets logged in after succesful sign up

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,17 +1,10 @@
-/**
- * UserController
- *
- * @description :: Server-side logic for managing users
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
-
 module.exports = {
 	create: function(req, res) {
   User.create(req.body).exec(function(err, result){
     if (err) {
       //Handle Error
     }
-    return res.redirect('/login')
+		sails.controllers.auth.login(req, res)
   });
 }
 

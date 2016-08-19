d7e1656cea402cfdfb79530311228efcfa5833a0
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-19 14:21:36 -0700

    next button will go to next cat

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    console.log( "your jquery is working!" );
-});
-
-$('a.nextlink').click(function(event) {
-    event.preventDefault();
-    $('a.nextlink').parent().children('img').attr('src', 'boop');
-});

b50aa70bb1bbf1592671b2e441a5b7d35ca1675c
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-30 16:32:46 -0700

    Turns authentication on for all controller actions

--- config/policies.js
+++ config/policies.js
@@ -29,7 +29,7 @@ module.exports.policies = {
   '*': true,
 
   'CatsController': {
-    // '*': 'isAuthenticated'
+    '*': 'isAuthenticated'
   }
 
   /***************************************************************************

c66d96115b56345edb8962b2decf749eddb6ebd1
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-30 10:18:26 -0700

    adding production db environment vars

--- config/connections.js
+++ config/connections.js
@@ -63,6 +63,15 @@ module.exports.connections = {
     database: 'kittycats'
   },
 
+  mongoproduction: {
+    adapter: 'sails-mongo',
+    host: process.env.MONGO_HOST,
+    port: process.env.MONGO_PORT,
+    user: process.env.MONGO_USER,
+    password: process.env.MONGO_PASSWORD,
+    database: process.env.MONGO_DB
+  }
+
   /***************************************************************************
   *                                                                          *
   * PostgreSQL is another officially supported relational database.          *
